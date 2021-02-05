7491076f82f3f1b3399c3fa00ff57f4bf99ad1bb
CrispthoAlex <alex877@gmail.com>
2021-02-05 00:09:00 -0500

    Timertrigger and Timer scripts were added to Clocks control

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    float time = 0f;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        TimerText.text = string.Format("{1:0}:{0:00.00}", time % 60, time / 60);
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerTrigger : MonoBehaviour
+{
+    // public Timer timer;
+
+    // It's called when the Collider other has stopped touching the trigger
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            other.GetComponent<Timer>().enabled = true;
+        }
+    }
+}
