ba01bc97ba46efaaf8bb7f4def3db6ddde5ea761
CrispthoAlex <alex877@gmail.com>
2021-05-24 07:57:31 -0500

    AR Business Card was ended

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/OpenUrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void LinkedIn()
+    {
+        Application.OpenURL("https://linkedin/in/carmurrain");
+    }
+
+    public void GitHub()
+    {
+        Application.OpenURL("https://github.com/CrispthoAlex");
+    }
+
+    public void Twitter()
+    {
+        Application.OpenURL("https://twitter.com/CrispthoAlex");
+    }
+
+    public void Email()
+    {
+        Application.OpenURL("mailto:ing.alexander-murrain@hotmail.es");
+    }
+}
